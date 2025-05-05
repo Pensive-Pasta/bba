d50f3900102ad9667e921faed49587a3140031bc
PensivePasta <pensivepasta@gmail.com>
2025-05-05 17:20:22 +0100

    Add placeholder images and on scroll animations.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 export default {
   content: ["./src/**/*.{html,js,astro}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeInUp: {
+          '0%': { opacity: '0', transform: 'translateY(0.5rem)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        fadeInDown: {
+          '0%': { opacity: '0', transform: 'translateY(-0.5rem)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        fadeInLeft: {
+          '0%': { opacity: '0', transform: 'translateX(-0.5rem)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+        fadeInRight: {
+          '0%': { opacity: '0', transform: 'translateX(0.5rem)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        fadeInUp: 'fadeInUp 1.4s ease-out forwards',
+        fadeInDown: 'fadeInDown 1.4s ease-out forwards',
+        fadeInLeft: 'fadeInLeft 1.4s ease-out forwards',
+        fadeInRight: 'fadeInRight 1.4s ease-out forwards',
+      },
+    },
   },
   plugins: [],
 };
