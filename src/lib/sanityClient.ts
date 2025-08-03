b062a81ab80b931a2fff4b54f3532223c219ac30
PensivePasta <pensivepasta@gmail.com>
2025-08-03 22:50:24 +0100

    Update projectId sanity

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@sanity/client';
 
 export const sanity = createClient({
-  projectId: import.meta.env.SANITY_PROJECT_ID,
+  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: import.meta.env.SANITY_API_VERSION,
   useCdn: false, // useCdn: false when using tokens
