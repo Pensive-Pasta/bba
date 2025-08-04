94ebe43455020194e4226a5c4adf6b32cb3d9b92
PensivePasta <pensivepasta@gmail.com>
2025-08-04 21:21:53 +0100

    Add fetch for project slugs

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -12,3 +12,16 @@ export const getProjectsQuery = `*[_type == "project"] | order(year desc){
   type,
   "imageUrl": heroImage.asset->url
 }`;
+
+export const getProjectBySlugQuery = `
+  *[_type == "project" && slug.current == $slug][0]{
+    title,
+    postCode,
+    county,
+    year,
+    site,
+    type,
+    "imageUrl": heroImage.asset->url,
+    "altText": heroImage.altText
+  }
+`;
