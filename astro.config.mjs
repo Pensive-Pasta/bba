d1948610a1926bc99213ff1a9fda13894192fb44
PensivePasta <pensivepasta@gmail.com>
2026-07-10 16:03:50 +0100

    Release holding page setup.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: import.meta.env.PUBLIC_SITE_URL ?? "https://wearebba.co.uk",
-  integrations: [sitemap()],
 });
