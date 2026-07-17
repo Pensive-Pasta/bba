42e26a36a9397a1b6d4a3fe4c49981119aad093e
PensivePasta <pensivepasta@gmail.com>
2026-07-17 13:44:56 +0100

    Catch all redirects

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { defineMiddleware } from "astro:middleware";
+
+const passthroughPrefixes = [
+  "/_astro/",
+  "/@",
+  "/src/",
+  "/node_modules/",
+  "/images/",
+  "/icons/",
+  "/logos/",
+  "/scripts/",
+  "/videos/",
+];
+
+export const onRequest = defineMiddleware((context, next) => {
+  const { pathname } = context.url;
+  const isAsset =
+    pathname !== "/" &&
+    (pathname.split("/").at(-1)?.includes(".") ||
+      passthroughPrefixes.some((prefix) => pathname.startsWith(prefix)));
+
+  if (pathname !== "/" && !isAsset) {
+    return context.redirect("/", 302);
+  }
+
+  return next();
+});
